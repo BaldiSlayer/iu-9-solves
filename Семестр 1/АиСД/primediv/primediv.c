6cb2136e6afdbc5bdcec28f98367d4656cdc25f1
Alexey <77489413+Dhhdhdjsjssjsj@users.noreply.github.com>
2022-12-09 03:18:53 +0300

    Update primediv.c

--- Семестр 1/АиСД/primediv/primediv.c
+++ Семестр 1/АиСД/primediv/primediv.c
@@ -9,64 +9,114 @@
                                                         |___/
  */
 
+#pragma GCC optimize("unroll-loops", "O3")
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int get(unsigned int * sieve, int i) {
-    return ((sieve[i >> 5] >> (i & 31)) & 1);
-}
-
-void set0(unsigned int * sieve, int i) {
-    sieve[i >> 5] &= ~(1u << (i & 31));
-}
+/*long long max(long long a, long long b) {
+    if (a > b) {
+        return a;
+    }
+    return b;
+}*/
 
 int main() {
     long long n;
     scanf("%lld", &n);
 
-    if (n < 0) {
-        n = -n;
+    n = llabs(n);
+
+    if (n == 2147483648) {
+        printf("%d", 2);
+        return 0;
     }
 
     int prime = 1;
-    for (size_t i = 2; i * i <= n; ++i) {
+    for (long long i = 2; i * i <= n; ++i) {
         if (n % i == 0) {
             prime = 0;
             break;
         }
     }
 
-    if (prime) {
-        printf("%ld\n", n);
+    if (prime == 1) {
+        printf("%lld", n);
         return 0;
     }
 
+    // строим решето Эратосфена
+    unsigned int sieve_size = n / 2 + 1;
+
     long long ans = 1;
 
-    // строим решето Эратосфена
-    long long sieve_size = n / 2 + 1;
+    unsigned int* sieve = malloc((unsigned int)((sieve_size / 32 + 1) * sizeof(unsigned int)));
+    memset(sieve, -1, (unsigned int)((sieve_size / 32 + 1) * sizeof(unsigned int)));
 
-    unsigned int* sieve = malloc((unsigned long long)((sieve_size / 32 + 1) * sizeof(unsigned int)));
-    memset(sieve, -1, (unsigned long long)((sieve_size / 32 + 1) * sizeof(unsigned int)));
+    unsigned long long i = 2;
+    while (n % i == 0) {
+        // max
+        if (i > ans) {
+            ans = i;
+        }
 
-    for (long long i = 2; i < sieve_size; ++i) {
-        if (get(sieve, i)) {
-            if (n % i == 0) {
-                ans = i;
+        n /= i;
+    }
+    for (unsigned long long j = i * i; j < sieve_size; j += i) {
+        sieve[j >> 5] &= ~(1u << (j & 31));
+    }
+
+    for (i = 3; i * i <= sieve_size; i += 2) {
+        if (((sieve[i >> 5] >> (i & 31)) & 1)) {
+            while (n % i == 0) {
+                if (i > ans) {
+                    ans = i;
+                }
+
+                n /= i;
             }
 
-            if (i*i > n) {
-                continue;
-            }
-
-            for (long long j = 1ll * i * i; j < sieve_size; j += i) {
-                set0(sieve, j);
+            for (unsigned long long j = i * i; j < sieve_size; j += 2*i) {
+                sieve[j >> 5] &= ~(1u << (j & 31));
             }
         }
     }
 
-    printf("%ld\n", ans);
+    for (int i = 2; (i * i <= n); ++i) {
+        // is prime n
+        if ((n < (sieve_size / 32 + 1)) && ((sieve[n >> 5] >> (n & 31)) & 1)) {
+            printf("%lld", n);
+            free(sieve);
+            return 0;
+        }
+
+        if ((n % i == 0) && ((sieve[(n / i) >> 5] >> ((n / i) & 31)) & 1)) {
+            printf("%lld", n / i);
+            free(sieve);
+            return 0;
+        }
+
+        while ((n % i == 0) && ((sieve[i >> 5] >> (i & 31)) & 1)) {
+            if (i > ans) {
+                ans = i;
+            }
+
+            n /= i;
+
+            if (ans == 1) {
+                printf("%lld", ans);
+                free(sieve);
+                return 0;
+            }
+        }
+    }
+
+    if ((n != 1) && (n > ans)) {
+        ans = n;
+    }
+
+    printf("%lld", ans);
 
     free(sieve);
 
