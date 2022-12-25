c415c0420d6048c5ef1b56701a6d1c10dd71b3e0
Alexey <77489413+Dhhdhdjsjssjsj@users.noreply.github.com>
2022-12-25 15:37:43 +0300

    Update lab7+.py

--- Семестр 1/Основы информатики/labs/lab7+.py
+++ Семестр 1/Основы информатики/labs/lab7+.py
@@ -1,20 +1,20 @@
 def mem(func, *args):
     if not args in d:
-        d[args] = test(args)
+        d[args] = func(args)
     return d[args]
 
-def test(*args):
+def f(*args):
     print("I'm counting....")
     return sum(list(args[0]))
 
 d = dict()
-print(mem(test, 1, 3))
-print(mem(test, 1, 2))
-print(mem(test, 1, 3))
-print(mem(test, 1, 3))
-print(mem(test, 1, 3))
-print(mem(test, 1, 3))
-print(mem(test, 1, 3))
-print(mem(test, 1, 3))
-print(mem(test, 1, 3))
-print(mem(test, 1, 3))
+print(mem(f, 1, 3))
+print(mem(f, 1, 2))
+print(mem(f, 1, 3))
+print(mem(f, 1, 3))
+print(mem(f, 1, 3))
+print(mem(f, 1, 3))
+print(mem(f, 1, 3))
+print(mem(f, 1, 3))
+print(mem(f, 1, 3))
+print(mem(f, 1, 3))
