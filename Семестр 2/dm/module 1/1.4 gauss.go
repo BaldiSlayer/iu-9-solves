55dfde70d77063c6a417163ab88e73d13adad49a
Alexey <77489413+BaldiSlayer@users.noreply.github.com>
2023-04-19 23:27:51 +0300

    Update 1.4 gauss.go

--- Семестр 2/dm/module 1/1.4 gauss.go
+++ Семестр 2/dm/module 1/1.4 gauss.go
@@ -1,86 +1,162 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"math"
 )
 
-// Helper function to print a matrix
-func printMatrix(matrix [][]float64) {
-    for i := 0; i < len(matrix); i++ {
-        for j := 0; j < len(matrix[i]); j++ {
-            fmt.Printf("%6.2f ", matrix[i][j])
-        }
-        fmt.Println()
-    }
-    fmt.Println()
+type Fraction struct {
+	n int
+	d int
 }
 
-// Helper function to swap two rows in a matrix
-func swapRows(matrix [][]float64, row1, row2 int) {
-    temp := matrix[row1]
-    matrix[row1] = matrix[row2]
-    matrix[row2] = temp
+func Divide(f1 Fraction, f2 Fraction) Fraction {
+	return Reduce(Fraction{f1.n * f2.d, f1.d * f2.n})
 }
 
-// Helper function to perform row reduction on a matrix
-func rowReduce(matrix [][]float64) {
-    numRows := len(matrix)
-    numCols := len(matrix[0])
-
-    // Perform Gaussian elimination on the matrix
-    for i := 0; i < numRows; i++ {
-        // Find the row with the largest absolute value in the current column
-        maxIndex := i
-        for j := i + 1; j < numRows; j++ {
-            if matrix[j][i] > matrix[maxIndex][i] {
-                maxIndex = j
-            }
-        }
-
-        // Swap the current row with the row containing the largest absolute value
-        if maxIndex != i {
-            swapRows(matrix, i, maxIndex)
-        }
-
-        // Normalize the current row
-        pivot := matrix[i][i]
-        for j := i; j < numCols; j++ {
-            matrix[i][j] /= pivot
-        }
-
-        // Eliminate all other entries in the current column
-        for j := 0; j < numRows; j++ {
-            if j != i {
-                factor := matrix[j][i]
-                for k := i; k < numCols; k++ {
-                    matrix[j][k] -= factor * matrix[i][k]
-                }
-            }
-        }
-    }
+func Multiply(f1 Fraction, f2 Fraction) Fraction {
+	return Reduce(Fraction{f1.n * f2.n, f1.d * f2.d})
 }
 
-// Main function to solve a system of linear equations using the Gaussian elimination method
+func Add(f1 Fraction, f2 Fraction) Fraction {
+	return Reduce(Fraction{f1.n*f2.d + f2.n*f1.d, f1.d * f2.d})
+}
+
+func Subtract(f1 Fraction, f2 Fraction) Fraction {
+	return Reduce(Fraction{f1.n*f2.d - f2.n*f1.d, f1.d * f2.d})
+}
+
+func Reduce(f Fraction) Fraction {
+	gcd := Gcd(f.n, f.d)
+	f.n /= gcd
+	f.d /= gcd
+	return f
+}
+
+func Greater(f1 Fraction, f2 Fraction) bool {
+	return int(math.Abs(float64(f1.n)))*f2.d > int(math.Abs(float64(f2.n)))*f1.d
+}
+
+func Gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func checkCramer(n int, a [][]Fraction, b []Fraction) bool {
+	if len(a) != n || len(a[0]) != n || len(b) != n {
+		return false
+	}
+
+	m := make([][]Fraction, n)
+	for i := range m {
+		m[i] = make([]Fraction, n+1)
+		copy(m[i], a[i])
+		m[i][n] = b[i]
+	}
+
+	rankA := gaussianElimination(n, n+1, m)
+	rankAugmented := gaussianElimination(n, n, a)
+
+	return rankA == rankAugmented && rankA == n
+}
+
+func gaussianElimination(n, m int, a [][]Fraction) int {
+	rank := 0
+	for row := 0; row < n; row++ {
+		pivot := row
+		for i := row + 1; i < n; i++ {
+			if Greater(a[i][row], a[pivot][row]) {
+				pivot = i
+			}
+		}
+		if pivot != row {
+			a[row], a[pivot] = a[pivot], a[row]
+		}
+		for i := row + 1; i < n; i++ {
+			coef := Divide(a[i][row], a[row][row])
+			for j := row; j < m; j++ {
+				a[i][j] = Subtract(a[i][j], Multiply(coef, a[row][j]))
+			}
+		}
+		rank++
+	}
+	return rank
+}
+
+func Gauss(a [][]Fraction, b []Fraction) (x []Fraction, success bool) {
+	n := len(b)
+	x = make([]Fraction, n)
+
+	arr2 := make([][]Fraction, len(a))
+	for i := range a {
+		arr2[i] = make([]Fraction, len(a[i]))
+		copy(arr2[i], a[i])
+	}
+
+	brr2 := make([]Fraction, len(b))
+	copy(brr2, b)
+	if !checkCramer(n, arr2, brr2) {
+		fmt.Println("a")
+		return x, false
+	}
+
+	for k := 0; k < n-1; k++ {
+		for i := k + 1; i < n; i++ {
+			if a[i][k].n != 0 {
+				lambda := Divide(a[i][k], a[k][k])
+				for j := k + 1; j < n; j++ {
+					a[i][j] = Subtract(a[i][j], Multiply(lambda, a[k][j]))
+				}
+				b[i] = Subtract(b[i], Multiply(lambda, b[k]))
+			}
+		}
+	}
+	for i := n - 1; i >= 0; i-- {
+		sum := b[i]
+		for j := i + 1; j < n; j++ {
+			sum = Subtract(sum, Multiply(a[i][j], x[j]))
+		}
+		x[i] = Divide(sum, a[i][i])
+	}
+	return x, true
+}
+
 func main() {
-    // Define the system of equations as a matrix
-    matrix := [][]float64{{1, 2, 3, 7},
-                          {4, 5, 6, 8},
-                          {7, 8, 9, 9}}
+	var n int
+	fmt.Scan(&n)
 
-    // Print the matrix before row reduction
-    fmt.Println("Matrix before row reduction:")
-    printMatrix(matrix)
+	a := make([][]Fraction, n)
+	for i := range a {
+		a[i] = make([]Fraction, n)
+		for j := range a[i] {
+			a[i][j] = Fraction{0, 1}
+		}
+	}
 
-    // Perform row reduction on the matrix
-    rowReduce(matrix)
+	b := make([]Fraction, n)
+	for i := 0; i < n; i++ {
+		b[i] = Fraction{0, 1}
+	}
 
-    // Print the matrix after row reduction
-    fmt.Println("Matrix after row reduction:")
-    printMatrix(matrix)
-
-    // Print the solution to the system of equations
-    fmt.Println("Solution:")
-    for i := 0; i < len(matrix); i++ {
-        fmt.Printf("x%d = %6.2f\n", i+1, matrix[i][len(matrix[i])-1])
-    }
+	temp := 0
+	for i := 0; i < n; i++ {
+		for j := 0; j < n+1; j++ {
+			fmt.Scan(&temp)
+			if j == n {
+				b[i].n = temp
+			} else {
+				a[i][j].n = temp
+			}
+		}
+	}
+	x, success := Gauss(a, b)
+	if success {
+		for _, val := range x {
+			fmt.Printf("%d/%d\n", val.n, val.d)
+		}
+	} else {
+		fmt.Println("No solution")
+	}
 }
